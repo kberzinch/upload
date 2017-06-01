615d07dc6da4fb30a43433802bbf4b509e994db3
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-05-31 20:27:20 -0500

    Semicolon
    (thanks StyleCI) (again)

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['url'] = 'https://upload.example.com'
+$config['url'] = 'https://upload.example.com';
 $config['target_directory'] = 'uploads';
 $config['files_are_accessible'] = true;
 $config['email']['from'] = 'uploads@example.com';
